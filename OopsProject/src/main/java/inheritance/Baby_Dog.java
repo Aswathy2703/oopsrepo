ae74369860c0aa4e3676696eb4b126d35fe0f6cd
ASWATHY <ASWATHY@LAPTOP-DFPKM2R8>
2024-08-21 10:09:42 +0530

    Single Inheritance

---
+++ OopsProject/src/main/java/inheritance/Baby_Dog.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Baby_Dog extends Dog{
+
+public void sleep()
+{
+System.out.println("Sleeping");
+}
+	public static void main(String[] args) {
+	Baby_Dog object=new Baby_Dog();
+	object.sleep();
+	
+object.run();
+	}
+
+}
---
+++ OopsProject/src/main/java/inheritance/Dog.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class Dog {
+public void run()
+{
+	System.out.println("Running");
+}
+}
